480afe333c6e972cb01a40337e4c6964ddfa6407
Siheng Wei <siheng.w@northeastern.edu>
2021-09-23 12:15:57 -0700

    Clicky Clicky

---
+++ app/src/main/java/edu/neu/madcourse/numad21fa_sihengwei/ClickyClickyActivity.java
@@ -0,0 +1,60 @@
+package edu.neu.madcourse.numad21fa_sihengwei;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+public class ClickyClickyActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicky_clicky);
+
+        //setOnTouchListener for the bottons
+        findViewById(R.id.btn_A).setOnTouchListener(this);
+        findViewById(R.id.btn_B).setOnTouchListener(this);
+        findViewById(R.id.btn_C).setOnTouchListener(this);
+        findViewById(R.id.btn_D).setOnTouchListener(this);
+        findViewById(R.id.btn_E).setOnTouchListener(this);
+        findViewById(R.id.btn_F).setOnTouchListener(this);
+    }
+
+    @SuppressLint({"NonConstantResourceId", "ClickableViewAccessibility"})
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        TextView tv_showPressed = findViewById(R.id.tv_showPressed);
+        switch (motionEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                switch (view.getId()) {
+                    case R.id.btn_A:
+                        tv_showPressed.setText(R.string.a);
+                        break;
+                    case R.id.btn_B:
+                        tv_showPressed.setText(R.string.b);
+                        break;
+                    case R.id.btn_C:
+                        tv_showPressed.setText(R.string.c);
+                        break;
+                    case R.id.btn_D:
+                        tv_showPressed.setText(R.string.d);
+                        break;
+                    case R.id.btn_E:
+                        tv_showPressed.setText(R.string.e);
+                        break;
+                    case R.id.btn_F:
+                        tv_showPressed.setText(R.string.f);
+                        break;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                tv_showPressed.setText(R.string.pressed);
+                break;
+        }
+        return true;
+    }
+}
--- app/src/main/java/edu/neu/madcourse/numad21fa_sihengwei/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_sihengwei/MainActivity.java
@@ -2,7 +2,8 @@ package edu.neu.madcourse.numad21fa_sihengwei;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.View;
@@ -13,36 +14,37 @@ import android.widget.Toast;
 
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  // onClick Event of the btn_convert
+  public void convertCurrency(View view) {
+    EditText dollarText = findViewById(R.id.dollarText);
+    TextView textView = findViewById(R.id.textView);
+    if (!dollarText.getText().toString().equals("")) {
+      float dollarValue = Float.parseFloat(dollarText.getText().toString());
+      float euroValue = dollarValue * 0.85F;
+      textView.setText(String.format(Locale.ENGLISH, "%f", euroValue));
+    } else {
+      textView.setText(R.string.no_value_string);
     }
+  }
 
-    // onClick Event of the btn_convert
-    public void convertCurrency(View view) {
-        EditText dollarText = findViewById(R.id.dollarText);
-        TextView textView = findViewById(R.id.textView);
-        if (!dollarText.getText().toString().equals("")) {
-            float dollarValue = Float.parseFloat(dollarText.getText().toString());
-            float euroValue = dollarValue * 0.85F;
-            textView.setText(String.format(Locale.ENGLISH,"%f", euroValue));
-        } else {
-            textView.setText(R.string.no_value_string);
-        }
+  @SuppressLint("NonConstantResourceId")
+  @Override
+  public void onClick(View view) {
+    switch (view.getId()) {
+      case R.id.btn_about:
+        Toast.makeText(
+            getApplicationContext(), "Siheng Wei\nsiheng.w@northeastern.edu", Toast.LENGTH_SHORT).show();
+        break;
+      case R.id.btn_clikyCliky:
+        startActivity(new Intent(this, ClickyClickyActivity.class));
     }
-
-    // onClick Event of the btn_about
-    public void displayProfile(View view) {
-
-        Context context = getApplicationContext();
-        CharSequence text = "Siheng Wei\nsiheng.w@northeastern.edu";
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-    }
-
+  }
 }
